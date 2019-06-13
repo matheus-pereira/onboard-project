dbbea82097897aa2175c85be5d479d9a1e26802c
Matheus da Silva Pereira <matheus.s.pereira@avanade.com>
2019-06-13 13:13:21 -0300

    Initial commit

---
+++ src/main/java/santander/systeam/onboard/OnboardProjectApplication.java
@@ -0,0 +1,13 @@
+package santander.systeam.onboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardProjectApplication.class, args);
+	}
+
+}
